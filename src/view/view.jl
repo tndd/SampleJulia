89c54fe76fbac048161b6b7cc3b564818abc061c
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-12 00:49:21 +0900

    この形式はかなり理想に近い

--- src/view/view.jl
+++ src/view/view.jl
@@ -1,7 +1,7 @@
 module View
 
-using ..Asset.Alpaca1: print_alpaca1
-using ..Asset.Alpaca2: print_alpaca2
+using SampleJulia.Asset.Alpaca1: print_alpaca1
+using SampleJulia.Asset.Alpaca2: print_alpaca2
 
 export show_view
 
