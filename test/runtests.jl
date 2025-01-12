cc0bfd30358c540a2ea2fd25f2112a1f07a85aaf
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-12 13:49:44 +0900

    テスト構成の変更

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,2 @@
-using TimerOutputs
-
-
-const TIMEROUTPUT = TimerOutput()
-
-@timeit TIMEROUTPUT "View Tests" include("view_test.jl")
-@timeit TIMEROUTPUT "Asset Tests" include("asset_test.jl")
-
-# 計測結果を表示
-show(TIMEROUTPUT)
+include("view/view_test.jl")
+include("asset/asset_test.jl")
