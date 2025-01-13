f722f1f880b5c31be9774ed08d2734a6272da342
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-13 23:18:31 +0900

    新形式のテストをassetにも導入

--- src/asset/alpaca/alpaca1.jl
+++ src/asset/alpaca/alpaca1.jl
@@ -5,3 +5,12 @@ module Alpaca1
         return "This is Alpaca1!"
     end
 end # module
+
+
+module Alpaca1Test
+    using TestItems
+
+    @testitem "print_alpaca1" begin
+        @test Asset.Alpaca1.print_alpaca1() == "This is Alpaca1!"
+    end
+end
--- src/asset/alpaca/alpaca2.jl
+++ src/asset/alpaca/alpaca2.jl
@@ -5,3 +5,12 @@ module Alpaca2
         return "This is Alpaca2!"
     end
 end # module
+
+
+module Alpaca2Test
+    using TestItems
+
+    @testitem "print_alpaca2" begin
+        @test Asset.Alpaca2.print_alpaca2() == "This is Alpaca2!"
+    end
+end
