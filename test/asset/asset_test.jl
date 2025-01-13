b17a3740b79525090a5b262eb3edda98866d8531
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-01-14 01:48:14 +0900

    古い形式のテストの削除

--- test/asset/asset_test.jl
+++
@@ -1,7 +0,0 @@
-# using SampleJulia.Asset: Alpaca1, Alpaca2
-# using Test
-
-# @testset "Asset Tests" begin
-#     @test Alpaca1.print_alpaca1() == "This is Alpaca1!"
-#     @test Alpaca2.print_alpaca2() == "This is Alpaca2!"
-# end
